2631647e97ba457740740eae39285f35b56cd6cb
Anwesh43 <anweshthecool0@gmail.com>
2018-08-31 10:22:52 +0530

    adding CircleIncLineView class

--- circleinclineview/src/main/java/com/anwesh/uiprojects/circleinclineview/CircleIncLineView.kt
+++ circleinclineview/src/main/java/com/anwesh/uiprojects/circleinclineview/CircleIncLineView.kt
@@ -10,3 +10,21 @@ import android.graphics.Canvas
 import android.content.Context
 
 val nodes : Int = 5
+
+class CircleIncLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
