d3e7472defde07cefad0b15e1d61337b0185c732
Anwesh43 <anweshthecool0@gmail.com>
2018-08-31 11:11:11 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcircleinclineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcircleinclineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcircleinclineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.circleinclineview.CircleIncLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleIncLineView.create(this)
     }
 }
