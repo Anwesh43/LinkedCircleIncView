70e5feebce2cd67f4530b2b0349aa9ed37327395
Anwesh43 <anweshthecool0@gmail.com>
2018-08-31 10:20:02 +0530

    adding libraries and defining nodes

--- circleinclineview/src/main/java/com/anwesh/uiprojects/circleinclineview/CircleIncLineView.kt
+++ circleinclineview/src/main/java/com/anwesh/uiprojects/circleinclineview/CircleIncLineView.kt
@@ -3,3 +3,10 @@ package com.anwesh.uiprojects.circleinclineview
 /**
  * Created by anweshmishra on 31/08/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+
+val nodes : Int = 5
